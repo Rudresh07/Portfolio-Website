862a167e543e386a859104b8e4058db0eaedfb1d
Rudresh07 <rudreshpatel01bhu10@gmail.com>
2024-02-04 15:11:12 +0530

    First Commit

---
+++ My Portfolio web/assets/js/android.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/LTMJq5mjvOZIER8X/scene.splinecode');
